22e667f45e4e8c0a87f45841100a71fec8a867eb
niczyji <niczyji@gmail.com>
2019-02-19 13:47:09 +0100

    Component Counting app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class Button extends React.Component {
+  handleClick = () => {
+  this.props.onClickFunction(this.props.incrementValue);
+  };
+   
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      return (
+      <button 
+      onClick={this.handleClick}>
+      +{this.props.incrementValue}
+      </button>
+  );
+}
+}
+
+const Result = (props) => {
+return (
+  <div>{props.counter}</div>
+  );
+};
+
+class App extends React.Component {
+  state = { counter: 0};
+
+  incrementCounter = (incrementValue) => {
+   this.setState((prevState) => ({
+    counter: prevState.counter + incrementValue
+  }));
+  };
+  
+  
+  render() {
+  return (
+    <div>
+      <Button incrementValue={1} onClickFunction= {this.incrementCounter}/>
+      <Button incrementValue={5} onClickFunction= {this.incrementCounter}/>
+      <Button incrementValue={10} onClickFunction= {this.incrementCounter}/>
+      <Button incrementValue={100} onClickFunction= {this.incrementCounter}/>
+      <Result counter={this.state.counter}/>
+    </div>
+      );
   }
 }
+
+
 
 export default App;
